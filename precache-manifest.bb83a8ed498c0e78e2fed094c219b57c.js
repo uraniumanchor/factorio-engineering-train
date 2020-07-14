5f7c52fbee468aebf08e76db5fdab4c95ebc3c2e
Benjamin Cutler <uraniumanchor@users.noreply.github.com>
2020-07-13 22:52:58 -0600

    Updates

---
+++ precache-manifest.bb83a8ed498c0e78e2fed094c219b57c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e292bcbb65708760b5835bfefa0704",
+    "url": "/factorio-engineering-train/index.html"
+  },
+  {
+    "revision": "b4da394f44820b4d3b0e",
+    "url": "/factorio-engineering-train/static/css/main.9c59633c.chunk.css"
+  },
+  {
+    "revision": "bb6a6cdd48f52647618e",
+    "url": "/factorio-engineering-train/static/js/2.650fd21c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/factorio-engineering-train/static/js/2.650fd21c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4da394f44820b4d3b0e",
+    "url": "/factorio-engineering-train/static/js/main.a38e9a8c.chunk.js"
+  },
+  {
+    "revision": "c288de9fb1ad5a54a345",
+    "url": "/factorio-engineering-train/static/js/runtime-main.5cbf440f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/factorio-engineering-train/precache-manifest.bb83a8ed498c0e78e2fed094c219b57c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/factorio-engineering-train/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
